dd3aabfc77dc6085ae2d3fa791042f3394075ba5
bilalturkmen <turkmenbilal@gmail.com>
2022-05-01 01:41:19 +0300

    added modal open and close script code

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// Define variables with queryselector
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Set the open function
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Set the close function
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Click event for open the modal
+btnsOpenModal.forEach(e => {
+  e.addEventListener('click', openModal);
+});
+
+// Click event for close the modal
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// Keydown event for close the modal when pressed Escape button
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
